45c259cb905ee98d08f64f67c5001871196dbcee
dysco72 <jeffreyscp087@gmail.com>
2022-06-21 01:48:42 -0700

    MyInfo page, Change CSS on sildebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,3 +1,8 @@
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
 import { Link } from "react-router-dom";
 import "./Sidebar.css"
 import PersonIcon from '@mui/icons-material/Person';
@@ -6,6 +11,23 @@ import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import TableViewIcon from '@mui/icons-material/TableView';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
+const ColorButton = styled(Button)(() => ({
+    width: 'fix-content',
+    textAlign: 'left',
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    lineHeight: 1.5,
+    color: '#000000',
+    backgroundColor: '##FBFBFF',
+    '&:hover': {
+        backgroundColor: '#E4E4FA',
+    },
+    '&:active': {
+        backgroundColor: '#E4E4FA',
+    },
+}));
+
 export default function Sidebar() {
     return (
         <div className="sidebar">
@@ -13,58 +35,72 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">My Dashboard</h3>
                     <ul className="sidebarList">
-                        <Link to="/" className="link">
-                            <li className="sidebarListItem">
-                                <PersonIcon className="sidebarIcon" />
-                                My Info
-                            </li>
-                        </Link>
-                        <Link to="/LogActivity" className="link">
-                            <li className="sidebarListItem">
-                                <TimelineIcon className="sidebarIcon" />
-                                Log Activity
-                            </li>
-                        </Link>
-                        <Link to="/ActivityHistory" className="link">
-                            <li className="sidebarListItem">
-                                <TrendingUpIcon className="sidebarIcon" />
-                                Activity History
-                            </li>
-                        </Link>
+                        <ColorButton variant="text">
+                            <Link to="/" className="link">
+                                <li className="sidebarListItem">
+                                    <PersonIcon className="sidebarIcon" />
+                                    My Info
+                                </li>
+                            </Link>
+                        </ColorButton>
+                        <ColorButton variant="text">
+                            <Link to="/LogActivity" className="link">
+                                <li className="sidebarListItem">
+                                    <TimelineIcon className="sidebarIcon" />
+                                    Log Activity
+                                </li>
+                            </Link>
+                        </ColorButton>
+                        <ColorButton variant="text">
+                            <Link to="/ActivityHistory" className="link">
+                                <li className="sidebarListItem">
+                                    <TrendingUpIcon className="sidebarIcon" />
+                                    Activity History
+                                </li>
+                            </Link>
+                        </ColorButton>
                     </ul>
                 </div>
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Admin Dashboard</h3>
                     <ul className="sidebarList">
-                        <Link to="/ViewTables" className="link">
-                            <li className="sidebarListItem">
-                                <TableViewIcon className="sidebarIcon" />
-                                View Tables
-                            </li>
-                        </Link>
-                        <Link to="/Analytics" className="link">
-                            <li className="sidebarListItem">
-                                <TimelineIcon className="sidebarIcon" />
-                                Analytics
-                            </li>
-                        </Link>
-                        <Link to="/AddEmployee" className="link">
-                            <li className="sidebarListItem">
-                                <PersonAddIcon className="sidebarIcon" />
-                                Add Employee
-                            </li>
-                        </Link>
+                        <ColorButton variant="text">
+                            <Link to="/ViewTables" className="link">
+                                <li className="sidebarListItem">
+                                    <TableViewIcon className="sidebarIcon" />
+                                    View Tables
+                                </li>
+                            </Link>
+                        </ColorButton>
+                        <ColorButton variant="text">
+                            <Link to="/Analytics" className="link">
+                                <li className="sidebarListItem">
+                                    <TimelineIcon className="sidebarIcon" />
+                                    Analytics
+                                </li>
+                            </Link>
+                        </ColorButton>
+                        <ColorButton variant="text">
+                            <Link to="/AddEmployee" className="link">
+                                <li className="sidebarListItem">
+                                    <PersonAddIcon className="sidebarIcon" />
+                                    Add Employee
+                                </li>
+                            </Link>
+                        </ColorButton>
                     </ul>
                 </div>
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Partner Dashboard</h3>
                     <ul className="sidebarList">
-                        <Link to="/ViewTable" className="link">
-                            <li className="sidebarListItem">
-                                <TableViewIcon className="sidebarIcon" />
-                                View Table (name only)
-                            </li>
-                        </Link>
+                        <ColorButton variant="text">
+                            <Link to="/ViewTable" className="link">
+                                <li className="sidebarListItem">
+                                    <TableViewIcon className="sidebarIcon" />
+                                    View Table
+                                </li>
+                            </Link>
+                        </ColorButton>
                     </ul>
                 </div>
             </div>
--- src/pages/MyDashboard/MyInfo/MyInfo.js
+++ src/pages/MyDashboard/MyInfo/MyInfo.js
@@ -1,9 +1,197 @@
 import "./MyInfo.css"
 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import WcIcon from '@mui/icons-material/Wc';
+import StraightenIcon from '@mui/icons-material/Straighten';
+import ScaleIcon from '@mui/icons-material/Scale';
+
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import SpeedOutlinedIcon from '@mui/icons-material/SpeedOutlined';
+import AirOutlinedIcon from '@mui/icons-material/AirOutlined';
+import DownhillSkiingOutlinedIcon from '@mui/icons-material/DownhillSkiingOutlined';
+import HolidayVillageOutlinedIcon from '@mui/icons-material/HolidayVillageOutlined';
+import WorkHistoryOutlinedIcon from '@mui/icons-material/WorkHistoryOutlined';
+
+import PublishIcon from '@mui/icons-material/Publish';
+
+import {client} from '../../../index'
+async function getUser() {
+    const userResponse = await client.entities.user.get('0181747f-3b8d-d2ac-139b-3052b7e850b7');
+    const name = userResponse.name; 
+    const age = userResponse.age;
+    const gender = userResponse.gender;
+    const height = userResponse.height;
+    const weight = userResponse.weight;
+    const temperature = userResponse.temperature;
+    const pulse = userResponse.pulse;
+    const pressure = userResponse.pressure;
+    const respiration = userResponse.respiration;
+    const exercise = userResponse.exercise;
+    const vacation = userResponse.vacation;
+    const work = userResponse.work;
+}
+
 export default function MyInfo() {
     return(
         <div className="myInfo">
-            my info
+        <div className="userTitleContainer">
+          <h1 className="userTitle">My Info</h1>
         </div>
+        <div className="userContainer">
+          <div className="userShow">
+            <div className="userShowTop">
+              <div className="userPhoto"><AccountCircleIcon /></div>
+              <div className="userShowTopTitle">
+                <span className="userShowUsername">Anna Becker</span>
+              </div>
+            </div>
+            <div className="userWrapper">
+                <div className="userLeft">
+                    <span className="userShowTitle">Personal Information</span>
+                    {/* name */}
+                    <div className="userShowInfo">
+                        <PermIdentityIcon className="userShowIcon" />
+                        Name: 
+                        <span className="userShowInfoTitle">annabeck99</span>
+                    </div>
+                    {/* age */}
+                    <div className="userShowInfo">
+                        <CalendarTodayIcon className="userShowIcon" />
+                        Age:
+                        <span className="userShowInfoTitle">10.12.1999</span>
+                    </div>
+                    {/* gender */}
+                    <div className="userShowInfo">
+                        <WcIcon className="userShowIcon" />
+                        Gender: 
+                        <span className="userShowInfoTitle">10.12.1999</span>
+                    </div>
+                    {/* height */}
+                    <div className="userShowInfo">
+                        <StraightenIcon className="userShowIcon" />
+                        Height: 
+                        <span className="userShowInfoTitle">10.12.1999</span>
+                    </div>
+                    {/* weight */}
+                    <div className="userShowInfo">
+                        <ScaleIcon className="userShowIcon" />
+                        Weight: 
+                        <span className="userShowInfoTitle">10.12.1999</span>
+                    </div>
+                </div>
+
+                <div className="userRight">
+                    <span className="userShowTitle">Health Details</span>
+                    {/* pemperature */}
+                    <div className="userShowInfo">
+                        <ThermostatIcon className="userShowIcon" />
+                        Temperature: 
+                        <span className="userShowInfoTitle">+1 123 456 67</span>
+                    </div>
+                    {/* pulse */}
+                    <div className="userShowInfo">
+                        <TimelineOutlinedIcon className="userShowIcon" />
+                        Pulse: 
+                        <span className="userShowInfoTitle">annabeck99@gmail.com</span>
+                    </div>
+                    {/* pressure */}
+                    <div className="userShowInfo">
+                        <SpeedOutlinedIcon className="userShowIcon" />
+                        Pressure: 
+                        <span className="userShowInfoTitle">New York | USA</span>
+                    </div>
+                    {/* respiration */}
+                    <div className="userShowInfo">
+                        <AirOutlinedIcon className="userShowIcon" />
+                        Respiration: 
+                        <span className="userShowInfoTitle">New York | USA</span>
+                    </div>
+                    {/* exercise */}
+                    <div className="userShowInfo">
+                        <DownhillSkiingOutlinedIcon className="userShowIcon" />
+                        Exercise: 
+                        <span className="userShowInfoTitle">New York | USA</span>
+                    </div>
+                    {/* vacation */}
+                    <div className="userShowInfo">
+                        <HolidayVillageOutlinedIcon className="userShowIcon" />
+                        Vacation: 
+                        <span className="userShowInfoTitle">New York | USA</span>
+                    </div>
+                    {/* work */}
+                    <div className="userShowInfo">
+                        <WorkHistoryOutlinedIcon className="userShowIcon" />
+                        Work: 
+                        <span className="userShowInfoTitle">New York | USA</span>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div className="userUpdate">
+            <span className="userUpdateTitle">Edit</span>
+            <form className="userUpdateForm">
+              <div className="userUpdateLeft">
+                <div className="userUpdateItem">
+                  <label>Name</label>
+                  <input
+                    type="text"
+                    placeholder="annabeck99"
+                    className="userUpdateInput"
+                  />
+                </div>
+                <div className="userUpdateItem">
+                  <label>Full Name</label>
+                  <input
+                    type="text"
+                    placeholder="Anna Becker"
+                    className="userUpdateInput"
+                  />
+                </div>
+                <div className="userUpdateItem">
+                  <label>Email</label>
+                  <input
+                    type="text"
+                    placeholder="annabeck99@gmail.com"
+                    className="userUpdateInput"
+                  />
+                </div>
+                <div className="userUpdateItem">
+                  <label>Phone</label>
+                  <input
+                    type="text"
+                    placeholder="+1 123 456 67"
+                    className="userUpdateInput"
+                  />
+                </div>
+                <div className="userUpdateItem">
+                  <label>Address</label>
+                  <input
+                    type="text"
+                    placeholder="New York | USA"
+                    className="userUpdateInput"
+                  />
+                </div>
+              </div>
+              <div className="userUpdateRight">
+                <div className="userUpdateUpload">
+                  <img
+                    className="userUpdateImg"
+                    src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                    alt=""
+                  />
+                  <label htmlFor="file">
+                    <PublishIcon className="userUpdateIcon" />
+                  </label>
+                  <input type="file" id="file" style={{ display: "none" }} />
+                </div>
+                <button className="userUpdateButton">Update</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     )
 }
